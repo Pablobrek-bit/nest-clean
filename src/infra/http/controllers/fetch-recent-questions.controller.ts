87aeebcbafadd5a5699577e9eab3ba1fb44168bf
Pablo Henrique da Silva Andrade <pablobreak2017@alu.ufc.br>
2025-10-18 18:00:07 -0300

    feat: add QuestionPresenter to format question data for HTTP responses and update FetchRecentQuestionsController to handle results properly

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -3,6 +3,7 @@ import z from 'zod';
 import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
 import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 import { FetchRecentQuestionsUseCase } from '../../../domain/forum/application/use-cases/fetch-recent-questions';
+import { QuestionPresenter } from '../presenters/question-presenter';
 
 const pageQueryParamSchema = z.object({
   page: z.coerce.number().min(1).default(1),
@@ -21,8 +22,14 @@ export class FetchRecentQuestionsController {
     query: PageQueryParam,
   ) {
     const page = query.page;
-    const questions = await this.fetchRecentQuestions.execute({ page });
+    const result = await this.fetchRecentQuestions.execute({ page });
 
-    return { questions };
+    if (result.isLeft()) {
+      throw new Error('Could not fetch recent questions');
+    }
+
+    const questions = result.value.questions;
+
+    return { questions: questions.map(QuestionPresenter.toHTTP) };
   }
 }
